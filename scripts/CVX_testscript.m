376e39c1610c5539d1096563097b0793bef7e523
Thomas Mohren <tlmohren@uw.edu>
2017-05-22 13:37:13 -0700

    pushing data from more runs 18

--- scripts/CVX_testscript.m
+++ scripts/CVX_testscript.m
@@ -21,49 +21,46 @@ for j = 1:length(varParList)
         % adjust parameters for this set of iterations----------------------
         DataMat = zeros(par.rmodes,par.iter);
         SensMat = zeros(par.rmodes,par.rmodes,par.iter);
-
         for k = 1:length(par.varParNames)
             par.(par.varParNames{k}) = varParList(j).(par.varParNames{k});
         end
-
         for k = 1:par.iter
             % Generate strain with Euler-Lagrange simulation ----------
             strainSet = eulerLagrangeConcatenate( varParList(j).theta_dist , varParList(j).phi_dist ,par);
-
             % Apply neural filters to strain --------------------------
             [X,G] = neuralEncoding(strainSet, par );
-
             % Find accuracy and optimal sensor locations  ---------
-            [acc,sensors ] = sparseWingSensors( X,G, par);
+            [Xtrain, Xtest, Gtrain, Gtest] = rand_cross_val(X, G, par.trainFraction);
+            
+%%%%%%%%%%%%%%%%%%%%%%%%%%TESTSECTION%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            
+            sensors = sensorLocSSPOC(Xtrain,Gtrain,par);
+            
+%             sensors = 
+            acc = sensorLocClassify(  sensors,Xtrain,Gtrain,Xtest,Gtest );
+            
+%%%%%%%%%%%%%%%%%%%%%%%%%%TESTSECTION%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
     %         Store data in 3D matrix ----------------------------
             q = length(sensors);
             prev = length(find( DataMat(q, :) )  );
             DataMat(q, prev+1) = acc; 
             SensMat(q, 1:q,prev+1) = sensors ;    
-
             % Print accuracy in command window --------------------
             fprintf('W_trunc = %1.0f, q = %1.0f, giving accuracy =%4.2f \n',[par.wTrunc,q,acc])
         end
-
         % save data 
-        saveName = sprintf('Testfiles_pls_delete',...
+        saveName = sprintf('Testfiles_CVX',...
                             [par.theta_dist , par.phi_dist , par.xInclude , par.yInclude , par.SSPOCon , ...
                             par.STAwidth , par.STAshift , par.NLDshift , par.NLDsharpness , par.wTrunc ]); 
-
         saveName = [saveName,computer,'_',datestr(datetime('now'), 30),'.mat'];
         save(  ['data',filesep, saveName]  ,'DataMat','SensMat','par')
         fprintf('Runtime = %g[s], Saved as: %s \n',[toc,saveName]) 
     catch
         fprintf('Run %i failed\n', j); 
     end
-    
-    if mod(j, 1)==0,
-        system('git pull');
-        system('git add data/*.mat');
-        system(sprintf('git commit * -m "pushing data from more runs %i"', j));
-        system('git push');
-    end;
 end
 
 system('git pull');
