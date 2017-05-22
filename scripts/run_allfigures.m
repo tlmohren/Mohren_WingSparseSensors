6a0d738bf4d63f510ed08cfe83efe5c95a9e2be2
Thomas Mohren <tlmohren@uw.edu>
2017-05-22 11:22:22 -0700

    remove unused files

--- scripts/run_allfigures.m
+++
@@ -1,8 +0,0 @@
-% run all figures 
-
-run('run_Figure1_SSPOCvsRandom.m')
-run('run_Figure2A_ThetaDistVSPhiDist.m')
-run('run_Figure2B_PhiThetaHump.m')
-run('run_Figure2C_ThetaDistVSPhiDistXXYY.m')
-run('run_Figure3_variableSTA.m')
-run('run_Figure4_variableNLD.m')
