b0496a0817af66ad239743b50e374f5983217755
Thomas Mohren <tlmohren@uw.edu>
2017-05-25 08:56:07 -0700

    code adjustment

--- functions/sensorLocSSPOC_CVXtest.m
+++ functions/sensorLocSSPOC_CVXtest.m
@@ -5,26 +5,26 @@ function  [  sensors  ] = sensorLocSSPOC_CVXtest(  Xtrain,Gtrain , par)
     n =  size(Xtrain,1);
     classes = unique(Gtrain); 
     c = numel(classes); 
-    if par.SSPOCon == 1
+    if par.SSPOCon == 1 && par.wTrunc~= 0
     % LDA
         [w_r, Psi, centroid] = PCA_LDA(Xtrain, Gtrain, 'nFeatures',par.rmodes);
 
-        if par.wTrunc ~= 0 
-            [~,Iw ] =sort( abs(w_r) , 'descend');
-            %---optionlal-----------------------
-                [~,Sigma, ~] = svd(Xtrain, 'econ' );
-                sing_vals = diag(Sigma(1:length(w_r),1:length(w_r)));
-                
-                if par.singValsMult == 1
-                    [~,Iw]=sort(abs(w_r).*sing_vals,'descend');  
-                else
-                    [~,Iw]=sort(abs(w_r),'descend');  
-                end
-            % -------------------------
-            big_modes = Iw(1:par.wTrunc);
-            w_t = w_r(big_modes);
-            Psi = Psi(:,big_modes);
-        end
+%         if par.wTrunc ~= 0 
+        [~,Iw ] =sort( abs(w_r) , 'descend');
+        %---optionlal-----------------------
+            [~,Sigma, ~] = svd(Xtrain, 'econ' );
+            sing_vals = diag(Sigma(1:length(w_r),1:length(w_r)));
+
+            if par.singValsMult == 1
+                [~,Iw]=sort(abs(w_r).*sing_vals,'descend');  
+            else
+                [~,Iw]=sort(abs(w_r),'descend');  
+            end
+        % -------------------------
+        big_modes = Iw(1:par.wTrunc);
+        w_t = w_r(big_modes);
+        Psi = Psi(:,big_modes);
+%         end
         
 %%%%%%%%%%%%%%%%%%%%%%%%%%TESTSECTION%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
