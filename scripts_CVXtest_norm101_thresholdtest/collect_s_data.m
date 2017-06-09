7573748b6f67fa0f2370009b2b7d7d61aad39a43
Thomas Mohren <tlmohren@uw.edu>
2017-06-09 16:09:47 -0700

    collect_s script

--- scripts_CVXtest_norm101_thresholdtest/collect_s_data.m
+++ scripts_CVXtest_norm101_thresholdtest/collect_s_data.m
@@ -1,7 +1,40 @@
 
 
 
-
+clc;clear all;close all
 addpathFolderStructure()
 load(['data' filesep 'ParameterList_CVXtestscript'])
 par.varParNames = fieldnames(varParList_short);
+
+
+% n_s  = length(dir('s_files'));
+
+fileNames = dir('s_files');
+n_s  = length(fileNames);
+sMat = zeros(10,26*51);
+for j = 1:n_s
+    try
+        aa = load( ['s_files' filesep fileNames(j).name]);
+%         fileNames(j).name 
+%         a1 = findstr(fileNames(j).name,'wt');
+%         a2 = findstr(fileNames(j).name,'_');
+%         
+%         str2num(fileNames(j).name(a1+2:a2(2)-1))
+%         
+%         
+%         b1 = findstr(fileNames(j).name,'CVX');
+% %         b2 = findstr(fileNames(j).name,'_');
+%         str2num(fileNames(j).name(b1+3))
+        
+        sMat( j,:)  = aa.s;
+        
+%         fileNames(j).name(5
+    end
+
+end
+
+%         saveName = sprintf('s_wt%g_dT%g_dP%g_CVX%g', par.wTrunc,  par.theta_dist , par.phi_dist,par.CVXcase ); 
+%         save(  ['s_files',filesep, saveName '_',datestr(datetime('now'), 30),'.mat']  ,'s')
+
+figure();
+histogram( abs(sMat(:)),100);axis([0,3,0,size(sMat,1)])
