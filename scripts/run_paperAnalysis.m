a91b875aaffc8c912ffc506363dda081c7857e9f
Thomas Mohren <tlmohren@uw.edu>
2017-07-03 12:39:04 -0700

    check on new git sync code

--- scripts/run_paperAnalysis.m
+++ scripts/run_paperAnalysis.m
@@ -35,38 +35,38 @@ for j = 1:length(varParList)
     end
     
     % Run parameter combination for a set number of iterations ---------
-%     for k = 1:par.iter
-%         try 
-%             par.curIter = k; 
-%             % Generate strain with Euler-Lagrange simulation ----------
-%             strainSet = eulerLagrangeConcatenate_predictTrain( varParList(j).theta_dist , varParList(j).phi_dist ,par);
-% 
-%             % Apply neural filters to strain --------------------------
-%             [X,G] = neuralEncoding(strainSet, par );
-% 
-%             % Find accuracy and optimal sensor locations  ---------
-%             [acc,sensors ] = sparseWingSensors( X,G, par);
-% 
-%             % Store data in 3D matrix ----------------------------
-%             q = length(sensors);
-%             prev = length(find( DataMat(q, :) )  );
-%             DataMat(q, prev+1) = acc; 
-%             SensMat(q, 1:q,prev+1) = sensors ;    
-% 
-%             % Print accuracy in command window --------------------
-%             fprintf('W_trunc = %1.0f, q = %1.0f, giving accuracy =%4.2f \n',[par.wTrunc,q,acc])
-%         catch
-%             fprintf(['W_trunc = %1.0f, gave error, length q = ',num2str(length(q)),' \n'],[par.wTrunc])
-%         end
-%     end
-% 
-%     % save classification accuracy and sensor location in small .mat file
-%     saveNameBase = sprintf(['Data_',par.saveNameParameters, '_dT%g_dP%g_xIn%g_yIn%g_sOn%g_STAw%g_STAs%g_NLDs%g_NLDg%g_wT%g_'],...
-%                         [par.theta_dist , par.phi_dist , par.xInclude , par.yInclude , par.SSPOCon , ...
-%                         par.STAwidth , par.STAshift , par.NLDshift , par.NLDsharpness , par.wTrunc ]);      
-%     saveName = [saveNameBase,computer,'_',datestr(datetime('now'), 30),'.mat'];
-%     save(  ['data',filesep, saveName]  ,'DataMat','SensMat','par')
-%     fprintf('Runtime = %g[s], Saved as: %s \n',[toc,saveName]) 
+    for k = 1:par.iter
+        try 
+            par.curIter = k; 
+            % Generate strain with Euler-Lagrange simulation ----------
+            strainSet = eulerLagrangeConcatenate_predictTrain( varParList(j).theta_dist , varParList(j).phi_dist ,par);
+
+            % Apply neural filters to strain --------------------------
+            [X,G] = neuralEncoding(strainSet, par );
+
+            % Find accuracy and optimal sensor locations  ---------
+            [acc,sensors ] = sparseWingSensors( X,G, par);
+
+            % Store data in 3D matrix ----------------------------
+            q = length(sensors);
+            prev = length(find( DataMat(q, :) )  );
+            DataMat(q, prev+1) = acc; 
+            SensMat(q, 1:q,prev+1) = sensors ;    
+
+            % Print accuracy in command window --------------------
+            fprintf('W_trunc = %1.0f, q = %1.0f, giving accuracy =%4.2f \n',[par.wTrunc,q,acc])
+        catch
+            fprintf(['W_trunc = %1.0f, gave error, length q = ',num2str(length(q)),' \n'],[par.wTrunc])
+        end
+    end
+
+    % save classification accuracy and sensor location in small .mat file
+    saveNameBase = sprintf(['Data_',par.saveNameParameters, '_dT%g_dP%g_xIn%g_yIn%g_sOn%g_STAw%g_STAs%g_NLDs%g_NLDg%g_wT%g_'],...
+                        [par.theta_dist , par.phi_dist , par.xInclude , par.yInclude , par.SSPOCon , ...
+                        par.STAwidth , par.STAshift , par.NLDshift , par.NLDsharpness , par.wTrunc ]);      
+    saveName = [saveNameBase,computer,'_',datestr(datetime('now'), 30),'.mat'];
+    save(  ['data',filesep, saveName]  ,'DataMat','SensMat','par')
+    fprintf('Runtime = %g[s], Saved as: %s \n',[toc,saveName]) 
     
     % Sync to github(git required) once every 100 parameter combinations or if last combination is reached 
     if ~(mod(j, 100)~= 0 && j ~= length(varParList))
